3af12466635b3956e63e41dc907fb41924e2bf06
nosnhojn <nosnhojn@gmail.com>
2013-05-16 13:46:51 -0400

    dump checkpoint

--- test/mock_uvm_report/basic_unit_test.sv
+++ test/mock_uvm_report/basic_unit_test.sv
@@ -20,6 +20,7 @@ module basic_unit_test;
   // running the Unit Tests on
   //===================================
   basic my_basic;
+  string dump_act, dump_exp;
 
 
   //===================================
@@ -105,6 +106,49 @@ module basic_unit_test;
   `SVTEST_END(actual_error_actual_fatal_expect_error_expect_fatal)
 
 
+// `SVTEST(dump_has_header)
+//   dump_exp = dump_header();
+//   dump_act = uvm_report_mock::dump();
+//   `FAIL_IF(dump_act != dump_exp);
+// `SVTEST_END(dump_has_header)
+
+
+  `SVTEST(dump_returns_star_for_expect_anything)
+    uvm_report_mock::expect_error();
+    uvm_report_error("", "");
+    dump_exp = dump_header();
+    dump_exp = { dump_exp , "0:   EXPECTED =>      UVM_ERROR                    \"*\" \"*\"\n" };
+    dump_exp = { dump_exp , "     ACTUAL   =>      UVM_ERROR                     \"\" \"\"\n" };
+$display("%s", dump_exp);
+    dump_act = uvm_report_mock::dump();
+$display("%s", dump_act);
+ 
+    `FAIL_IF(dump_act != dump_exp);
+  `SVTEST_END(dump_returns_star_for_expect_anything)
+
+
+// `SVTEST(dump_truncates_ids_longer_than_20)
+// `SVTEST_END(dump_truncates_ids_longer_than_20)
+
+// uvm_report_mock::dump
+// 0:   EXPECTED => UVM_ERROR                         "*" "*"
+//      ACTUAL   => UVM_ERROR                          "" ""
+// 1:   EXPECTED => UVM_WARNING                       "*" "*"
+//      ACTUAL   => UVM_WARNING    "ID lkj lkjlkjl lkj l" "MSG"
+// 2:   EXPECTED => UVM_FATAL                        "ID" "MSG"
+//      ACTUAL   =>                <None reported>
+// 3:   EXPECTED =>                <None reported>
+//      ACTUAL   => UVM_WARNING                      "ID" "MSG"
+// 4:   EXPECTED => UVM_ERROR                        "ID" "MSG"
+//      ACTUAL   => UVM_ERROR                        "ID" "MSG"
+// 5:   EXPECTED => UVM_WARNING                      "ID" "MSG"
+//      ACTUAL   => UVM_ERROR                        "ID" "MSG"
+
+
   `SVUNIT_TESTS_END
+
+  function string dump_header();
+    return "uvm_report_mock::dump\n";
+  endfunction
 
 endmodule
