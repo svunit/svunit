46934ffd3620b035bfe7fbf8bac9eea193c59274
Tudor Timi <tudor.timi@gmail.com>
2015-05-17 14:35:05 +0200

    added tests for new "-t" option to buildSVUnit/runSVUnit

---
+++ test/frmwrk_27/test2_unit_test.sv
@@ -0,0 +1,66 @@
+`include "svunit_defines.svh"
+`include "test2.sv"
+
+module test2_unit_test;
+  import svunit_pkg::svunit_testcase;
+
+  string name = "test2_ut";
+  svunit_testcase svunit_ut;
+
+
+  //===================================
+  // This is the UUT that we're 
+  // running the Unit Tests on
+  //===================================
+  test2 my_test2;
+
+
+  //===================================
+  // Build
+  //===================================
+  function void build();
+    svunit_ut = new(name);
+
+    my_test2 = new(/* New arguments if needed */);
+  endfunction
+
+
+  //===================================
+  // Setup for running the Unit Tests
+  //===================================
+  task setup();
+    svunit_ut.setup();
+    /* Place Setup Code Here */
+  endtask
+
+
+  //===================================
+  // Here we deconstruct anything we 
+  // need after running the Unit Tests
+  //===================================
+  task teardown();
+    svunit_ut.teardown();
+    /* Place Teardown Code Here */
+  endtask
+
+
+  //===================================
+  // All tests are defined between the
+  // SVUNIT_TESTS_BEGIN/END macros
+  //
+  // Each individual test must be
+  // defined between `SVTEST(_NAME_)
+  // `SVTEST_END
+  //
+  // i.e.
+  //   `SVTEST(mytest)
+  //     <test code>
+  //   `SVTEST_END
+  //===================================
+  `SVUNIT_TESTS_BEGIN
+
+
+
+  `SVUNIT_TESTS_END
+
+endmodule
---
+++ test/frmwrk_28/test_unit_test.sv
@@ -0,0 +1,66 @@
+`include "svunit_defines.svh"
+`include "test.sv"
+
+module test_unit_test;
+  import svunit_pkg::svunit_testcase;
+
+  string name = "test_ut";
+  svunit_testcase svunit_ut;
+
+
+  //===================================
+  // This is the UUT that we're 
+  // running the Unit Tests on
+  //===================================
+  test my_test;
+
+
+  //===================================
+  // Build
+  //===================================
+  function void build();
+    svunit_ut = new(name);
+
+    my_test = new(/* New arguments if needed */);
+  endfunction
+
+
+  //===================================
+  // Setup for running the Unit Tests
+  //===================================
+  task setup();
+    svunit_ut.setup();
+    /* Place Setup Code Here */
+  endtask
+
+
+  //===================================
+  // Here we deconstruct anything we 
+  // need after running the Unit Tests
+  //===================================
+  task teardown();
+    svunit_ut.teardown();
+    /* Place Teardown Code Here */
+  endtask
+
+
+  //===================================
+  // All tests are defined between the
+  // SVUNIT_TESTS_BEGIN/END macros
+  //
+  // Each individual test must be
+  // defined between `SVTEST(_NAME_)
+  // `SVTEST_END
+  //
+  // i.e.
+  //   `SVTEST(mytest)
+  //     <test code>
+  //   `SVTEST_END
+  //===================================
+  `SVUNIT_TESTS_BEGIN
+
+
+
+  `SVUNIT_TESTS_END
+
+endmodule
