465578c9c05d3940823e15ee2512935d843afb33
nosnhojn <nosnhojn@gmail.com>
2018-08-09 13:27:20 -0600

    checkpoint

--- bin/wavedromSVUnit.py
+++ bin/wavedromSVUnit.py
@@ -1,15 +1,28 @@
 import re
+import json
 from os import listdir
 
 class WD:
     def __init__(self):
-        self.methods = []
+        self.method = []
 
     def process(self):
-        self.methods = [ WDMethod(f) for f in listdir('.') if re.match('.*\.json', f) ]
+        self.method = [ WDMethod(f) for f in listdir('.') if re.match('^[a-zA-Z_].*\.json$', f) ]
         
 
 class WDMethod:
     def __init__(self, ifile):
         self.ifile = ifile
-        print (self.ifile)
+        self.name = ''
+        self.clk = ''
+        self.signal = []
+        self.rawData = {}
+        self.process()
+
+    def process(self):
+        with open(self.ifile) as _input:
+            self.rawData = json.load(_input)
+
+        self.name = self.rawData['name']
+        self.clk = [ clk for clk in self.rawData['signal'] if re.match('p', clk['wave']) ][0]
+        self.signal = [ signal for signal in self.rawData['signal'] if not re.match('p', signal['wave']) ]
--- test/wavedrom_0/wavedrom-test.py
+++ test/wavedrom_0/wavedrom-test.py
@@ -8,6 +8,20 @@ from wavedromSVUnit import WD
 import re
 
 class BaseTest (unittest.TestCase):
+    def getWD(self, name):
+        _wd = [ wd for wd in self.wd.method if re.match(name, wd.name) ]
+        return _wd[0]
+
+    def getWave(self, name, signal):
+        try:
+            _signal = [ _signal for _signal in self.getWD(name).signal if re.match(signal, _signal['name']) ]
+            return _signal[0]['wave']
+        except IndexError:
+            return None
+
+    def getSignal(self, name):
+        return [ wd for wd in self.wd.method if re.match(name, wd.name) ][0]
+
     def setUp(self):
         self.wd = WD()
         self.wd.process()
@@ -16,9 +30,23 @@ class BaseTest (unittest.TestCase):
         pass
 
 class ParseTests (BaseTest):
-    def testJSONFind(self):
-        assert len([ wd for wd in self.wd.methods if re.match('.*\.json', wd.ifile) ]) > 0
-        assert len([ wd for wd in self.wd.methods if not re.match('.*\.json', wd.ifile) ]) == 0
+    def testJSONOnly(self):
+        assert len([ wd for wd in self.wd.method if re.match('.*\.json$', wd.ifile) ]) > 0
+        assert len([ wd for wd in self.wd.method if not re.match('.*\.json$', wd.ifile) ]) == 0
+
+    def testTaskNameFromJSON(self):
+        # wavedrom0 has name: 'task0'
+        assert self.getWD('task0') != None
+
+    def testGetSignalWave(self):
+        assert len(self.getWave('task1', 'psel')) == 2
+        assert self.getWave('task1', 'psel') == '01'
+
+    def testGetClock(self):
+        assert self.getWD('task1').clk['name'] == 'clk'
+
+    def testClockNotASignal(self):
+        assert self.getWave('task1', 'clk') == None
         
 
 if __name__ == "__main__":
