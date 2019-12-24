59844d0f0ae456b565678d7afb2038a5b4a00225
Tudor Timi <tudortimi@users.noreply.github.com>
2019-12-24 11:23:44 +0100

    Run pytest tests in temporary directories
    This way we avoid having to clean up the source tree, as any files that
    are created by the tooling are stored in '/tmp'.

---
+++ test/test_frmwrk.py
@@ -0,0 +1,33 @@
+import subprocess
+import pytest
+from test_utils import *
+
+
+FIXTURE_DIR = os.path.join(
+    os.path.dirname(os.path.realpath(__file__)),
+    )
+
+
+@pytest.mark.datafiles(os.path.join(FIXTURE_DIR, 'frmwrk_0', 'test.sv') )
+def test_frmwrk_0(datafiles):
+    with datafiles.as_cwd():
+        create_unit_test('test.sv')
+        subprocess.check_call(['buildSVUnit'])
+
+        golden_class_unit_test('test', 'test')
+        golden_testsuite_with_1_unittest('test')
+        golden_testrunner_with_1_testsuite()
+
+        verify_file('test_unit_test.gold', 'test_unit_test.sv')
+        verify_testsute('testsuite.gold')
+        verify_testrunner('testrunner.gold', '_')
+
+
+@pytest.mark.datafiles(os.path.join(FIXTURE_DIR, 'frmwrk_1', 'test.sv') )
+def test_frmwrk_1(datafiles):
+    with datafiles.as_cwd():
+        create_unit_test('test.sv')
+
+        golden_class_unit_test('test', 'test0')
+
+        verify_file('test_unit_test.gold', 'test_unit_test.sv')
