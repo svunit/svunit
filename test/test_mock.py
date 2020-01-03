675c18a6d9c42403497282ba15aa5a0e0a6eda8d
Tudor Timi <tudortimi@users.noreply.github.com>
2020-01-03 15:47:15 +0100

    Migrate mock_uvm_report_ius_uvm1.2 to pytest

--- test/test_mock.py
+++ test/test_mock.py
@@ -18,3 +18,12 @@ def test_mock_uvm_report_ius(datafiles):
             if s == 'irun':
                 subprocess.check_call(['runSVUnit', '-sim', s, '-uvm', '-define', 'UVM_NO_DEPRECATED', '-define', 'RUN_SVUNIT_WITH_UVM_REPORT_MOCK'])
                 expect_testrunner_pass('run.log')
+
+
+@all_files_in_dir('mock_uvm_report_ius_uvm1.2')
+def test_mock_uvm_report_ius_uvm1_2(datafiles):
+    with datafiles.as_cwd():
+        for s in get_simulators():
+            if s == 'irun':
+                subprocess.check_call(['runSVUnit', '-sim', s, '-uvm', '-define', 'UVM_NO_DEPRECATED', '-c_arg', '-uvmhome $INCISIV_HOME/tools/methodology/UVM/CDNS-1.2/sv', '-define', 'RUN_SVUNIT_WITH_UVM_REPORT_MOCK'])
+                expect_testrunner_pass('run.log')
