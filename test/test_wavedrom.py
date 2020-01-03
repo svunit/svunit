d5b15ff892aa33d652ad1744a81d40aa142153b4
Tudor Timi <tudortimi@users.noreply.github.com>
2020-01-03 16:02:06 +0100

    Migrate wavedrom_1 to pytest

--- test/test_wavedrom.py
+++ test/test_wavedrom.py
@@ -6,3 +6,11 @@ from utils import *
 def test_wavedrom_0(datafiles):
     with datafiles.as_cwd():
         subprocess.check_call(['python3', 'wavedrom-test.py'])
+
+
+@all_files_in_dir('wavedrom_1')
+def test_wavedrom_1(datafiles):
+    with datafiles.as_cwd():
+        for s in get_simulators():
+            subprocess.check_call(['runSVUnit', '-s', s, '-w'])
+            expect_testrunner_pass('run.log')
