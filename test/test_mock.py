6487f98da7d0927aa9416a4d23aa83aa4b1bb64e
Tudor Timi <tudortimi@users.noreply.github.com>
2020-01-03 15:44:21 +0100

    Migrate mock_uvm_report_ius to pytest

--- test/test_mock.py
+++ test/test_mock.py
@@ -8,3 +8,13 @@ def test_mock_uvm_report(datafiles):
         for s in get_simulators():
             subprocess.check_call(['runSVUnit', '-sim', s, '-uvm', '-define', 'UVM_NO_DEPRECATED', '-define', 'RUN_SVUNIT_WITH_UVM_REPORT_MOCK'])
             expect_testrunner_pass('run.log')
+
+
+# TODO This is redundant with the test that loops over all simulators.
+@all_files_in_dir('mock_uvm_report_ius')
+def test_mock_uvm_report_ius(datafiles):
+    with datafiles.as_cwd():
+        for s in get_simulators():
+            if s == 'irun':
+                subprocess.check_call(['runSVUnit', '-sim', s, '-uvm', '-define', 'UVM_NO_DEPRECATED', '-define', 'RUN_SVUNIT_WITH_UVM_REPORT_MOCK'])
+                expect_testrunner_pass('run.log')
