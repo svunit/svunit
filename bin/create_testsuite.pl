a407b6796a40cdb72404a6f125e0280305a38adc
Tudor Timi <tudortimi@users.noreply.github.com>
2020-08-16 11:59:27 +0200

    Fix filtering of 'static' keyword in 'create_testsuite.pl'
    No idea why keywords have to be filtered here. It's safer to just leave
    it in for now.

--- bin/create_testsuite.pl
+++ bin/create_testsuite.pl
@@ -150,7 +150,7 @@ sub getUnitTests() {
         }
 
         # filter out the static/automatic keywords
-        s/static//;
+        s/(\s+)(static)(\s+)/$1$3/;
         s/automatic//;
 
         if ( /^\s*module\s*(\w+_unit_test);/ ) {
