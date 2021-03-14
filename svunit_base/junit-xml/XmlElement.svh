10d1dcc08cd82a8e1dd9042dafb9dd135457a5a6
Tudor Timi <tudortimi@users.noreply.github.com>
2021-03-14 22:24:10 +0100

    Add workaround for Xcelium issue that messes up indentation

--- svunit_base/junit-xml/XmlElement.svh
+++ svunit_base/junit-xml/XmlElement.svh
@@ -46,8 +46,12 @@ class XmlElement;
   local function string as_string_with_indent(string indent);
     string result;
     result = $sformatf("%s<%s>", indent, get_start_tag_contents);
-    foreach (children[i])
-      result = { result, "\n", children[i].as_string_with_indent({ indent, "  " }) };
+    foreach (children[i]) begin
+      // XXX WORKAROUND Xcelium messes up the indentation if we try to inline the following
+      // variable.
+      string child_indent = { indent, "  " };
+      result = { result, "\n", children[i].as_string_with_indent(child_indent) };
+    end
     result = { result, "\n", $sformatf("%s</%s>", indent, tag) };
     return result;
   endfunction
