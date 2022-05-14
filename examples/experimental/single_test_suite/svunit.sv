7b763a72696b7f0078e05229776a1a37125dbbe7
Tudor Timi <tudortimi@users.noreply.github.com>
2022-05-14 20:18:26 +0300

    Register test builders instead of just test names
    We would like to either pass the test type or at least the constructor. Since we can't pass around types or functions, we have to encapsulate the test constructor into an object we can pass.

--- examples/experimental/single_test_suite/svunit.sv
+++ examples/experimental/single_test_suite/svunit.sv
@@ -1,29 +1,50 @@
 package svunit;
 
-  typedef string string_darray[];
-
-
   class test;
 
-    protected static string test_names[$];
+    typedef class builder;
+    typedef builder builder_darray[];
 
-    protected static function bit register_test_name(string test_name);
-      test_names.push_back(test_name);
+    protected static builder test_builders[$];
+
+    protected static function bit register_test_builder(builder b);
+      test_builders.push_back(b);
       return 1;
     endfunction
 
-    static function string_darray get_test_names();
-      return test_names;
+    static function builder_darray get_test_builders();
+      return test_builders;
     endfunction
+
+    virtual class builder;
+      pure virtual function test create();
+    endclass
+
+    class concrete_builder #(type T = test) extends builder;
+      local static concrete_builder #(T) single_instance;
+
+      static function concrete_builder #(T) get();
+        if (single_instance == null)
+          single_instance = new();
+        return single_instance;
+      endfunction
+
+      virtual function T create();
+        T result = new();
+        return result;
+      endfunction
+    endclass
 
   endclass
 
 
   function automatic void run_all_tests();
-    string test_names[] = test::get_test_names();
+    test::builder test_builders[] = test::get_test_builders();
     $display("Found the following tests:");
-    foreach (test_names[i])
-      $display("  %s", test_names[i]);
+    foreach (test_builders[i]) begin
+      test t = test_builders[i].create();
+      $display($vtypename(t));
+    end
   endfunction
 
 endpackage
--- examples/experimental/single_test_suite/svunit.svh
+++ examples/experimental/single_test_suite/svunit.svh
@@ -1,7 +1,8 @@
 `define TEST_BEGIN(TEST_NAME) \
   class TEST_NAME extends svunit::test; \
-    local static const bit is_test_name_registerd = register_test_name(`"TEST_NAME`");
+    local static const bit is_test_builder_registerd \
+        = register_test_builder(concrete_builder#(TEST_NAME)::get()); \
 
 
 `define TEST_END \
-  endclass
+  endclass \
