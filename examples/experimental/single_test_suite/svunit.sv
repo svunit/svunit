07795d2590ec58e4bde8ea51f1a316b4494001be
Tudor Timi <tudortimi@users.noreply.github.com>
2022-05-14 19:35:50 +0300

    Emulate run of found tests by printing their names
    Assuming we didn't make any mistakes in the code that registers, it seems that it's not enough to just compile a package. We probably have to also import, so it is part of the elaborated code.

--- examples/experimental/single_test_suite/svunit.sv
+++ examples/experimental/single_test_suite/svunit.sv
@@ -1,5 +1,8 @@
 package svunit;
 
+  typedef string string_darray[];
+
+
   class test;
 
     protected static string test_names[$];
@@ -9,6 +12,18 @@ package svunit;
       return 1;
     endfunction
 
+    static function string_darray get_test_names();
+      return test_names;
+    endfunction
+
   endclass
+
+
+  function automatic void run_all_tests();
+    string test_names[] = test::get_test_names();
+    $display("Found the following tests:");
+    foreach (test_names[i])
+      $display("  %s", test_names[i]);
+  endfunction
 
 endpackage
