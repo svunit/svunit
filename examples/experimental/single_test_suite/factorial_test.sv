ef831025b7121687f701f86cb66aa17b7f7015aa
Tudor Timi <tudortimi@users.noreply.github.com>
2022-05-14 19:02:24 +0300

    Compile test code

--- examples/experimental/single_test_suite/factorial_test.sv
+++ examples/experimental/single_test_suite/factorial_test.sv
@@ -1,18 +1,18 @@
 package factorial_test;
 
-  `include "svunit.svh"
+  // `include "svunit.svh"
 
 
-  `TEST_BEGIN(handles_zero_input)
-    `FAIL_UNLESS(factorial(0) == 1)
-  `TEST_END
+  // `TEST_BEGIN(handles_zero_input)
+  //   `FAIL_UNLESS(factorial(0) == 1)
+  // `TEST_END
 
 
-  `TEST_BEGIN(handles_positive_input)
-    `FAIL_UNLESS(factorial(1) == 1)
-    `FAIL_UNLESS(factorial(2) == 2)
-    `FAIL_UNLESS(factorial(3) == 6)
-    `FAIL_UNLESS(factorial(8) == 40320)
-  `TEST_END
+  // `TEST_BEGIN(handle_positive_input)
+  //   `FAIL_UNLESS(factorial(1) == 1)
+  //   `FAIL_UNLESS(factorial(2) == 2)
+  //   `FAIL_UNLESS(factorial(3) == 6)
+  //   `FAIL_UNLESS(factorial(8) == 40320)
+  // `TEST_END
 
 endpackage
