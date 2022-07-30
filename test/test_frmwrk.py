967932620944203c94d8f869a6b25a1f9d71e785
Tudor Timi <tudortimi@users.noreply.github.com>
2022-07-30 18:53:45 +0300

    Issue error message when absolute path passed to `--directory`
    This doesn't currently work. In most cases, users will probably want to use relative paths anyway.
    
    Exit code 4 is what pytest returns on command line usage errors.

--- test/test_frmwrk.py
+++ test/test_frmwrk.py
@@ -570,3 +570,15 @@ def test_does_not_collect_test_from_cwd_passed_with_directory_option(tmp_path):
 
         golden_testrunner_with_1_testsuite()
         verify_testrunner('testrunner.gold', '___test_dir1')
+
+
+def test_absolute_path_for_directory_option_issues_error(tmp_path):
+    tmp_path.joinpath('run_dir').mkdir()
+    tmp_path.joinpath('test_dir1').mkdir()
+
+    with working_directory(tmp_path/'run_dir'):
+        run_result = subprocess.run(['runSVUnit', '--directory', tmp_path.joinpath('test_dir1').resolve()], stdout=subprocess.PIPE)
+
+        assert run_result.returncode == 4
+        assert b'absolute paths' in run_result.stdout
+        assert b'not yet supported' in run_result.stdout
