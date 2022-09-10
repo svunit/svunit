2c66a63f749196630b1070651699b6fab8b1aac0
Tudor Timi <tudortimi@users.noreply.github.com>
2022-09-10 19:37:47 +0300

    Add implementation of `queue` class

--- examples/experimental/test_fixture/queue.sv
+++ examples/experimental/test_fixture/queue.sv
@@ -2,15 +2,21 @@ package queue;
 
   class queue #(type T = int);
 
+    local T q_[$];
+
+
     function void enqueue(T e);
+      q_.push_back(e);
     endfunction
 
 
     function T dequeue();
+      return q_.pop_front();
     endfunction
 
 
     function int unsigned size();
+      return q_.size();
     endfunction
 
   endclass
