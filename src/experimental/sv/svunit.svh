be3fa295d81123ced0dc51a9b7d0aca11631636f
Tudor Timi <tudortimi@users.noreply.github.com>
2022-09-11 17:23:43 +0300

    Extract macro from `TEST_BEGIN` and `TEST_F_BEGIN`

--- src/experimental/sv/svunit.svh
+++ src/experimental/sv/svunit.svh
@@ -1,42 +1,17 @@
+`include "svunit_internal.svh"
+
+
 `define TEST_BEGIN(TEST_NAME) \
-  class TEST_NAME extends svunit::test; \
-    local static const bit is_test_builder_registerd \
-        = register_test_builder(concrete_builder#(TEST_NAME)::get()); \
-    local static string full_name_of_class = $sformatf("%m"); \
-    \
-    virtual function string full_name(); \
-      return full_name_of_class; \
-    endfunction \
-    \
-    virtual function string name(); \
-      return `"TEST_NAME`"; \
-    endfunction \
-    \
-    virtual task run(); \
+  `SVUNIT_INTERNAL_TEST_BEGIN(TEST_NAME, svunit::test)
 
 
 `define TEST_END \
-    endtask \
-  endclass \
+  `SVUNIT_INTERNAL_TEST_END
 
 
 `define TEST_F_BEGIN(TEST_FIXTURE, TEST_NAME) \
-  class TEST_NAME extends TEST_FIXTURE; \
-    local static const bit is_test_builder_registerd \
-        = register_test_builder(concrete_builder#(TEST_NAME)::get()); \
-    local static string full_name_of_class = $sformatf("%m"); \
-    \
-    virtual function string full_name(); \
-      return full_name_of_class; \
-    endfunction \
-    \
-    virtual function string name(); \
-      return `"TEST_NAME`"; \
-    endfunction \
-    \
-    virtual task run(); \
+  `SVUNIT_INTERNAL_TEST_BEGIN(TEST_NAME, TEST_FIXTURE)
 
 
 `define TEST_F_END \
-    endtask \
-  endclass \
+  `SVUNIT_INTERNAL_TEST_END
