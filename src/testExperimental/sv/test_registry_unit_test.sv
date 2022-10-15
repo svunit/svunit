59f9b1b447f4847758370fb90e0211905e465c3e
Tudor Timi <tudortimi@users.noreply.github.com>
2022-10-15 21:40:25 +0300

    Register separate testsuite for each package

--- src/testExperimental/sv/test_registry_unit_test.sv
+++ src/testExperimental/sv/test_registry_unit_test.sv
@@ -46,24 +46,6 @@ module test_registry_unit_test;
     `SVTEST_END
 
 
-    `SVTEST(two_test_packages)
-      test_registry tr = new();
-      testsuite testsuites[];
-      testcase testcases[];
-
-      tr.register(fake_test_builder::new_instance(), "some_test_package.some_test");
-      tr.register(fake_test_builder::new_instance(), "some_other_test_package.some_test");
-
-      testsuites = tr.get_testsuites();
-      `FAIL_UNLESS(testsuites.size() == 1)
-
-      testcases = testsuites[0].get_testcases();
-      `FAIL_UNLESS(testcases.size() == 2)
-      `FAIL_UNLESS_STR_EQUAL(testcases[0].get_name(), "some_test_package")
-      `FAIL_UNLESS_STR_EQUAL(testcases[1].get_name(), "some_other_test_package")
-    `SVTEST_END
-
-
     `SVTEST(two_tests_under_package__registers_only_one_tc)
       test_registry tr = new();
       testsuite testsuites[];
@@ -115,6 +97,19 @@ module test_registry_unit_test;
       `FAIL_UNLESS(testcases.size() == 2)
       `FAIL_UNLESS_STR_EQUAL(testcases[0].get_name(), "some_test_class")
       `FAIL_UNLESS_STR_EQUAL(testcases[1].get_name(), "some_other_test_class")
+    `SVTEST_END
+
+
+    `SVTEST(two_packages_with_test_classes)
+      test_registry tr = new();
+      testsuite testsuites[];
+      testcase testcases[];
+
+      tr.register(fake_test_builder::new_instance(), "some_test_package.some_test_class.some_test");
+      tr.register(fake_test_builder::new_instance(), "some_other_test_package.some_test_class.some_test");
+
+      testsuites = tr.get_testsuites();
+      `FAIL_UNLESS(testsuites.size() == 2)
     `SVTEST_END
 
   `SVUNIT_TESTS_END
