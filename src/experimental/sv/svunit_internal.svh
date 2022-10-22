b627bbbf3e943957c83119e119930071d8f2cc3a
Tudor Timi <tudortimi@users.noreply.github.com>
2022-10-22 21:44:22 +0300

    Avoid cryptic QuestaSim optimization error by using full class name
    Even though `concrete_builder` is a class that is defined under `test` (generally the base class of all tests), QuestaSim can't seem to find it.

--- src/experimental/sv/svunit_internal.svh
+++ src/experimental/sv/svunit_internal.svh
@@ -2,7 +2,7 @@
   class TEST_NAME extends BASE_CLASS; \
     \
     local static const bit is_test_builder_registerd \
-        = register_test_builder(concrete_builder#(TEST_NAME)::get(), $typename(TEST_NAME)); \
+        = register_test_builder(svunit::test::concrete_builder#(TEST_NAME)::get(), $typename(TEST_NAME)); \
     \
     \
     virtual function string name(); \
