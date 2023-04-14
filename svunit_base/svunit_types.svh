60b820003a5626bc706662e4e5b789cf645851a0
Peter Monsson <peter@monsson.dk>
2023-04-14 19:58:36 +0000

    Fixing small errors

--- svunit_base/svunit_types.svh
+++ svunit_base/svunit_types.svh
@@ -21,11 +21,11 @@
     Enum: results_t
     enumerated type containing PASS/FAIL
   */
-  typedef enum {PASS=1, FAIL=0}  results_t;
+  typedef enum bit {PASS=1, FAIL=0}  results_t;
 
 
   /*
     Enum: boolean_t
     enumerated type containing TRUE/FALSE
   */
-  typedef enum {TRUE=1, FALSE=0} boolean_t;
+  typedef enum bit {TRUE=1, FALSE=0} boolean_t;
