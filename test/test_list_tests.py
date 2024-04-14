02241eb72f58f81ee1acc57992377c4aa8cc1986
Tudor Timi <tudortimi@users.noreply.github.com>
2024-04-14 21:55:38 +0300

    Streamline tests by changing directory in fixture that creates testing setup

--- test/test_list_tests.py
+++ test/test_list_tests.py
@@ -14,7 +14,7 @@ def test_list_tests_option_exists(tmp_path):
 
 
 @pytest.fixture(scope="function")
-def setup_with_one_test_case(tmp_path):
+def setup_with_one_test_case(tmp_path, monkeypatch):
     some_unit_test = tmp_path.joinpath('some_unit_test.sv')
     some_unit_test.write_text('''
 module some_unit_test;
@@ -48,57 +48,57 @@ module some_unit_test;
 endmodule
     ''')
 
-    return tmp_path
+    monkeypatch.chdir(tmp_path)
 
 
 @all_available_simulators()
 def test_that_tests_are_not_run_when_option_used(simulator, setup_with_one_test_case):
-    subprocess.check_call(['runSVUnit', '-s', simulator, '--list-tests'], cwd=setup_with_one_test_case)
-    log = setup_with_one_test_case.joinpath('run.log')
+    subprocess.check_call(['runSVUnit', '-s', simulator, '--list-tests'])
+    log = pathlib.Path('run.log')
     assert 'FAILED' not in log.read_text()
 
 
 @all_available_simulators()
 def test_that_test_cases_are_not_run_when_option_used(simulator, setup_with_one_test_case):
-    subprocess.check_call(['runSVUnit', '-s', simulator, '--list-tests'], cwd=setup_with_one_test_case)
+    subprocess.check_call(['runSVUnit', '-s', simulator, '--list-tests'])
 
-    with open(setup_with_one_test_case.joinpath('run.log'), 'r') as log:
+    with open(pathlib.Path('run.log'), 'r') as log:
         test_case_running = re.compile("some_ut.*RUNNING")
         assert not any(test_case_running.search(line) for line in log)
 
 
 @all_available_simulators()
 def test_that_test_suites_are_not_run_when_option_used(simulator, setup_with_one_test_case):
-    subprocess.check_call(['runSVUnit', '-s', simulator, '--list-tests'], cwd=setup_with_one_test_case)
+    subprocess.check_call(['runSVUnit', '-s', simulator, '--list-tests'])
 
-    with open(setup_with_one_test_case.joinpath('run.log'), 'r') as log:
+    with open(pathlib.Path('run.log'), 'r') as log:
         test_suite_running = re.compile("_ts.*RUNNING")
         assert not any(test_suite_running.search(line) for line in log)
 
 
 @all_available_simulators()
 def test_that_nothing_is_run_when_option_used(simulator, setup_with_one_test_case):
-    subprocess.check_call(['runSVUnit', '-s', simulator, '--list-tests'], cwd=setup_with_one_test_case)
-    log = setup_with_one_test_case.joinpath('run.log')
+    subprocess.check_call(['runSVUnit', '-s', simulator, '--list-tests'])
+    log = pathlib.Path('run.log')
     assert 'RUNNING' not in log.read_text()
 
 
 @all_available_simulators()
 def test_that_status_is_not_reported_when_option_used(simulator, setup_with_one_test_case):
-    subprocess.check_call(['runSVUnit', '-s', simulator, '--list-tests'], cwd=setup_with_one_test_case)
-    log = setup_with_one_test_case.joinpath('run.log')
+    subprocess.check_call(['runSVUnit', '-s', simulator, '--list-tests'])
+    log = pathlib.Path('run.log')
     assert 'PASSED' not in log.read_text()
 
 
 @all_available_simulators()
 def test_that_test_from_test_case_is_printed_when_option_used(simulator, setup_with_one_test_case):
-    subprocess.check_call(['runSVUnit', '-s', simulator, '--list-tests'], cwd=setup_with_one_test_case)
-    with open(setup_with_one_test_case.joinpath('run.log'), 'r') as log:
+    subprocess.check_call(['runSVUnit', '-s', simulator, '--list-tests'])
+    with open(pathlib.Path('run.log'), 'r') as log:
         assert sum(1 for line in log if line == "    some_test\n") == 1
 
 
 @all_available_simulators()
 def test_that_test_case_is_printed_when_option_used(simulator, setup_with_one_test_case):
-    subprocess.check_call(['runSVUnit', '-s', simulator, '--list-tests'], cwd=setup_with_one_test_case)
-    log = setup_with_one_test_case.joinpath('run.log')
+    subprocess.check_call(['runSVUnit', '-s', simulator, '--list-tests'])
+    log = pathlib.Path('run.log')
     assert "some_ut\n    some_test\n" in log.read_text()
