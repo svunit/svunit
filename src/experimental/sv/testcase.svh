5a98f68b335ba2e0f835f8519d8b6270194d9aeb
Tudor Timi <tudortimi@users.noreply.github.com>
2024-09-29 22:37:18 +0200

    Fix experimental code to work after internal refactoring of test execution classes

--- src/experimental/sv/testcase.svh
+++ src/experimental/sv/testcase.svh
@@ -4,7 +4,6 @@ class testcase extends svunit_testcase;
 
 
   local test::builder test_builders[$];
-  local test tests[$];
 
 
   function new(string name);
@@ -14,43 +13,12 @@ class testcase extends svunit_testcase;
 
   function void register(test::builder test_builder);
     test_builders.push_back(test_builder);
+    add_test(test_builder.create().get_adapter());
   endfunction
 
 
   function array_of_test_builder get_test_builders();
     return test_builders;
   endfunction
-
-
-  // FIXME Get tests to work again
-  /*
-  `SVUNIT_TESTS_BEGIN
-    foreach (test_builders[i]) begin
-      test t = test_builders[i].create();
-      tests.push_back(t);
-    end
-    foreach (tests[i]) begin
-      string test_name = tests[i].name();
-      svunit_testcase svunit_ut = this;
-
-      // More or less what `SVTEST expands to
-      if (svunit_pkg::_filter.is_selected(svunit_ut, test_name)) begin
-        string _testName = test_name;
-        integer local_error_count = svunit_ut.get_error_count();
-        string fileName;
-        int lineNumber;
-        `INFO($sformatf(`"%s::RUNNING`", _testName));
-        svunit_pkg::current_tc = svunit_ut;
-        svunit_ut.add_junit_test_case(_testName);
-        svunit_ut.start();
-        fork
-          begin
-            fork
-              begin
-                tests[i].run();
-      `SVTEST_END
-    end
-  `SVUNIT_TESTS_END
-  */
 
 endclass
